3b8a46ca700c621bec53dd0d1be187afe546044a
Sebastien Mauroo <sebastien.mauroo@outlook.com>
2023-05-16 22:44:00 +0200

    creation of streamlit toolkit library

--- Home.py
+++ Home.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pyrebase
 import pandas as pd
 import time
+import Streamlit_toolkit as sttk
+from PIL import Image
 
 ####PAGE CONFIG
 
@@ -11,126 +13,20 @@ st.set_page_config(
     layout='wide'
 )
 
-#### add CSS style and hide unneeded streamlit visuals
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
-####LOGIN CODE####
-
-
-if 'firebase' not in st.session_state:
-        st.session_state['firebase'] = pyrebase.initialize_app(st.secrets['FirebaseCredentials'])
-
-if 'userID' not in st.session_state:
-        st.session_state['userID'] = "Unknown"
-
-if 'logged_in' not in st.session_state:
-        st.session_state['logged_in'] = False
-
-if 'password_reset' not in st.session_state:
-        st.session_state['password_reset'] = False
-
-if 'create_account' not in st.session_state:
-        st.session_state['create_account'] = False
-
-if st.session_state['logged_in'] == False and st.session_state['create_account'] == False:  ###sign in screen 
-    with st.form("Inloggen"):
-        st.subheader("Login")
-        email = st.text_input("Emailadres")
-        pw = st.text_input("Wachtwoord", type="password")
-        login = st.form_submit_button("Login",type="primary")
+sttk.set_css_style("style.css")
+#sttk.firebase_login_screen_init()
     
 
-    if login:
-        try:
-            signin = st.session_state['firebase'].auth().sign_in_with_email_and_password(email,pw)
-            st.session_state['logged_in'] = True
-            st.session_state['password_reset'] = False
-            st.session_state['userID'] = signin['localId']
-        except:
-            st.error("verkeerd wachtwoord")
-            st.session_state['password_reset'] = True
-
-    if st.session_state['password_reset'] == True:
-        if st.button("Verander je wachtwoord"):
-            try:
-                st.session_state['firebase'].auth().send_password_reset_email(email)
-                st.markdown(f"Email verzonden naar {email}")
-                st.caption(f"Deze email kan ook in je spam folder te vinden zijn")
-            except:
-                st.info(f"Niet mogelijk om een email te sturen naar: {email}, probeer later opnieuw")
-    
-
-    ####button to show create account instead
-    if st.button("Account aanmaken"):
-        st.session_state['password_reset'] = False
-        st.session_state['create_account'] = True
-        st.experimental_rerun()
-
-    
-    ####rerun script to show real page after succesfull login
-    if st.session_state['logged_in'] == True:
-        st.experimental_rerun()
-    else:
-        st.stop()
-
-elif st.session_state['logged_in'] == False and st.session_state['create_account'] == True:  ###create password button
-    with st.form("Account aanmaken"):
-        st.subheader("Account aanmaken")
-        email = st.text_input("Emailadres")
-        pw = st.text_input("Wachtwoord", type="password")
-        login = st.form_submit_button("Enter",type="primary")
-
-    ####button to show login screen instead
-
-    if st.button("Inloggen met bestaand account"):
-        st.session_state['password_reset'] = False
-        st.session_state['create_account'] = False
-        st.experimental_rerun()
-
-    ####check email and password
-    if login:
-        if '@' not in email or '.' not in email:
-            st.warning("Vul een geldig emailadres in")
-            st.stop()
-        elif len(pw)<6:
-            st.info("Wachtwoord moet minimaal 7 karakters lang zijn")
-            st.stop()
-        else:
-            try:
-                signin = st.session_state['firebase'].auth().create_user_with_email_and_password(email,pw)
-                st.session_state['logged_in'] = True
-                st.session_state['password_reset'] = False
-                st.session_state['userID'] = signin['localId']
-            except:
-                st.info("Aanmaken van een account niet gelukt, probeer later opnieuw")
-                st.stop()
-        st.experimental_rerun()
-    else:
-        st.stop()
-    
-
-
-####################################
-###ACTUAL APP
-####################################
-
-####methods
 
 #####
+image = Image.open('artwork/metert logo.png')
+st.image(image,width=50,output_format="PNG")
 st.title("Welkom op uw MeterT Dashboard! 👋",anchor=False)
 metcol1,metcol2,metcol3 = st.columns(3)
 metcol1.metric("Energie verbruik",f"22 kWh","+2%")
 metcol2.metric("CO2 uitstoot",f"110 g","-25%")
 metcol3.metric("Energie injectie",f"115 kWh", "-13%")
+st.write("test")
 
 
 
